9e60f3913e9adf7f262567b677bab9e1771b60f4
Luis Antonio <tyluis20@gmail.com>
2020-08-17 17:42:01 -0300

    config version

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:Hexpenses/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(HExpenses());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
