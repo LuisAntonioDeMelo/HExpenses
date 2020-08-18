1cc9cba54985bce57f3dc13c86c46b81660ec499
Luis Antonio <tyluis20@gmail.com>
2020-08-18 00:08:02 -0300

    criando layout para base do projeto
     #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Hexpenses/models/Transacao.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,9 +18,85 @@ class HExpenses extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Init version'),
+    final _transactions = [
+      Transaction(
+        id: 't1',
+        title: 'Novo tenis',
+        value: 340.50,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't2',
+        title: 'Pagar conta',
+        value: 70,
+        date: DateTime.now(),
+      )
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Despesas Pessoais')),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Graficos'),
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      child: Text(
+                        'R\$ : ${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          tr.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
